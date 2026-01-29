660416042afb75128d9c3eeeb1ced1b0fd43fe8f
Zack289 <ozack911@gmail.com>
2026-01-29 23:38:12 +0545

    feat: update server configuration and add health check endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,22 +13,37 @@ import commentRoutes from "./route/commentRoutes";
 
 const app = express();
 
-
 app.use(cors({ origin: ENV.FRONTEND_URL, credentials: true }));
 // `credentials: true` allows the frontend to send cookies to the backend so that we can authenticate the user.
 app.use(clerkMiddleware()); // auth obj will be attached to the req
 app.use(express.json()); // parses JSON request bodies.
 app.use(express.urlencoded({ extended: true })); // parses form data (like HTML forms).
 
-
-app.get('/',(req,res)=>{
-    res.send("Hello from Home page")
+app.get("/api/health", (req, res) => {
+  res.json({
+    message: "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments",
+    },
+  });
 });
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/comments", commentRoutes);
 
-app.listen(ENV.PORT, ()=>{
-    console.log(`App is running in http://localhost:${ENV.PORT}`);
-})
+if (ENV.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+
+  // serve static files from frontend/dist
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // handle SPA routing - send all non-API routes to index.html - react app
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => console.log(`Server is up and running on PORT:`, ENV.PORT));
