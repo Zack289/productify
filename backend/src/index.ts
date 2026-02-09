91f62b29c415c636188e2e7ce5a41abb12a10000
Zack289 <ozack911@gmail.com>
2026-02-09 17:35:09 +0545

    deploying in process

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,21 +34,21 @@ app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/comments", commentRoutes);
 
-// Serve frontend in production
-if (ENV.NODE_ENV === "production") {
-  const __dirname = path.resolve();
+// // Serve frontend in production
+// if (ENV.NODE_ENV === "production") {
+//   const __dirname = path.resolve();
 
-  // Serve static files
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   // Serve static files
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // SPA fallback (send all non-API routes to React)
-  app.get("/*", (req, res) => {
-    // Avoid sending index.html for API routes
-    if (req.path.startsWith("/api")) {
-      return res.status(404).json({ error: "API route not found" });
-    }
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+//   // SPA fallback (send all non-API routes to React)
+//   app.get("/*", (req, res) => {
+//     // Avoid sending index.html for API routes
+//     if (req.path.startsWith("/api")) {
+//       return res.status(404).json({ error: "API route not found" });
+//     }
+//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+//   });
+// }
 
 app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
